d79ab44b7660d5f77c97c9d0b727d25ba91b137c
Sujeet Vishwakarma <sujeetkr.vishwakarma.eee16@itbhu.ac.in>
2020-02-12 15:36:12 +0530

    changed word tokenize to python3

--- data/scripts/nltk_scripts/word-tokenize.py
+++ data/scripts/nltk_scripts/word-tokenize.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys, nltk
 
 while 1:
 	line = sys.stdin.readline()
 	if not line:
 		break
-	line = line.decode('utf8')
-	print ' '.join(nltk.word_tokenize(line.strip())).encode('utf8')
+	print(' '.join(nltk.word_tokenize(line.strip())))
