af6fbc9d0fd75783635bbbb27198d06788749cb8
Sujeet Vishwakarma <sujeetkr.vishwakarma.eee16@itbhu.ac.in>
2020-02-12 15:36:50 +0530

    changed preprocess script

--- training/preprocess.sh
+++ training/preprocess.sh
@@ -4,9 +4,7 @@ set -x
 
 source ../paths.sh
 
-## paths to training and development datasets
-src_ext=src
-trg_ext=trg
+## paths to training and development datasets\
 train_data_prefix=$DATA_DIR/train
 dev_data_prefix=$DATA_DIR/dev
 dev_data_m2=$DATA_DIR/dev.all.m2
@@ -20,14 +18,17 @@ FAIRSEQPY=$SOFTWARE_DIR/fairseq-py
 # subword segmentation
 mkdir -p models/bpe_model
 bpe_operations=30000
-cat $train_data_prefix.tok.$trg_ext | $SUBWORD_NMT/learn_bpe.py -s $bpe_operations > models/bpe_model/train.bpe.model
+
+
+
+cat $train_data_prefix.tok.trg | $SUBWORD_NMT/learn_bpe.py -s $bpe_operations > models/bpe_model/train.bpe.model
 mkdir -p processed/
-$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $train_data_prefix.tok.$src_ext > processed/train.all.src
-$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $train_data_prefix.tok.$trg_ext > processed/train.all.trg
-$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $dev_data_prefix.tok.$src_ext > processed/dev.src
-$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $dev_data_prefix.tok.$trg_ext > processed/dev.trg
-cp $dev_data_m2 processed/dev.m2
-cp $dev_data_prefix.all.tok.$src_ext processed/dev.input.txt
+$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $train_data_prefix.tok.src > processed/train.all.src
+$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $train_data_prefix.tok.trg > processed/train.all.trg
+$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $dev_data_prefix.tok.src > processed/dev.src
+$SCRIPTS_DIR/apply_bpe.py -c models/bpe_model/train.bpe.model < $dev_data_prefix.tok.trg > processed/dev.trg
+#cp $dev_data_m2 processed/dev.m2
+#cp $dev_data_prefix.all.tok.src processed/dev.input.txt
 
 ##########################
 #  getting annotated sentence pairs only
@@ -38,5 +39,5 @@ cut -f2  processed/train.annotated.src-trg > processed/train.trg
 
 #########################
 # preprocessing
-python3.5 $FAIRSEQPY/preprocess.py --source-lang src --target-lang trg --trainpref processed/train --validpref processed/dev --testpref  processed/dev --nwordssrc 30000 --nwordstgt 30000 --destdir processed/bin
+python3 $FAIRSEQPY/preprocess.py --source-lang src --target-lang trg --trainpref processed/train --validpref processed/dev --testpref  processed/dev --nwordssrc 30000 --nwordstgt 30000 --destdir processed/bin
 
